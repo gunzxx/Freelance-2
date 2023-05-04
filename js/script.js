055829f3d339c218a45af903863f38f914490744
gunzxx <ggm5850@gmail.com>
2023-05-04 14:09:40 +0700

    step 1 done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$("#icon-hide").click(function(event) {
+    event.preventDefault();
+    if($('#show_hide_password input').attr("type") == "text"){
+        $('#show_hide_password input').attr('type', 'password');
+        $('#icon-hide').html('<i class="far fa-eye"></i>');
+    }else if($('#show_hide_password input').attr("type") == "password"){
+        $('#show_hide_password input').attr('type', 'text');
+        $('#icon-hide').html('<i class="far fa-eye-slash"></i>');
+    }
+});
+$('#no_pendaftaran').inputmask("9{4}.[A|9]{3}.9{5}.9{4}.9{3}");
